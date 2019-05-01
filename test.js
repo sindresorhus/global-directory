0aae4d64887f77af94c4fb7ca12aa9f93c0a8291
Dimitri Benin <BendingBender@users.noreply.github.com>
2019-05-01 06:19:05 +0000

    Add TypeScript definition (#9)

--- test.js
+++ test.js
@@ -1,27 +1,27 @@
 import test from 'ava';
 import execa from 'execa';
-import globalDirs from '.';
+import globalDirectories from '.';
 
-console.log(globalDirs);
+console.log(globalDirectories);
 
 const npm = arguments_ => execa.stdout('npm', arguments_);
 
 test('npm.prefix', async t => {
-	t.is(globalDirs.npm.prefix, await npm(['prefix', '--global']));
+	t.is(globalDirectories.npm.prefix, await npm(['prefix', '--global']));
 });
 
 test('npm.packages', async t => {
-	t.is(globalDirs.npm.packages, await npm(['root', '--global']));
+	t.is(globalDirectories.npm.packages, await npm(['root', '--global']));
 });
 
 test('npm.binaries', async t => {
-	t.is(globalDirs.npm.binaries, await npm(['bin', '--global']));
+	t.is(globalDirectories.npm.binaries, await npm(['bin', '--global']));
 });
 
 test('yarn', async t => {
 	await npm(['install', '--global', 'yarn']);
-	t.truthy(globalDirs.yarn);
-	t.truthy(globalDirs.yarn.prefix);
-	t.truthy(globalDirs.yarn.packages);
-	t.truthy(globalDirs.yarn.binaries);
+	t.truthy(globalDirectories.yarn);
+	t.truthy(globalDirectories.yarn.prefix);
+	t.truthy(globalDirectories.yarn.packages);
+	t.truthy(globalDirectories.yarn.binaries);
 });
